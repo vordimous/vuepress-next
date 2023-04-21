f1f91344fbc6fe655ce01e461f650087626683ab
Mr.Hope <mister-hope@outlook.com>
2023-04-21 12:47:52 +0800

    fix(client): avoid updating existing head tags, close #1268

--- packages/shared/src/utils/resolveHeadIdentifier.ts
+++ packages/shared/src/utils/resolveHeadIdentifier.ts
@@ -1,20 +1,21 @@
 import type { HeadConfig } from '../types/index.js'
 
+const onlyTags = ['title', 'base']
+const allowedTags = ['link', 'meta', 'script', 'style', 'noscript', 'template']
+
 /**
  * Resolve identifier of a tag, to avoid duplicated tags in `<head>`
  */
-export const resolveHeadIdentifier = ([
-  tag,
-  attrs,
-  content,
-]: HeadConfig): string => {
+export const resolveHeadIdentifier = ([tag, attrs, content]: HeadConfig):
+  | string
+  | null => {
   // avoid duplicated `<meta>` with same `name`
   if (tag === 'meta' && attrs.name) {
     return `${tag}.${attrs.name}`
   }
 
   // there should be only one `<title>` or `<base>`
-  if (['title', 'base'].includes(tag)) {
+  if (onlyTags.includes(tag)) {
     return tag
   }
 
@@ -23,5 +24,27 @@ export const resolveHeadIdentifier = ([
     return `${tag}.${attrs.id}`
   }
 
-  return JSON.stringify([tag, attrs, content])
+  if (allowedTags.includes(tag)) {
+    return JSON.stringify([
+      tag,
+      Object.fromEntries(
+        Object.entries(attrs)
+          // handle boolean attributes
+          .map(([key, value]) =>
+            typeof value === 'boolean'
+              ? value
+                ? [key, '']
+                : null
+              : [key, value]
+          )
+          .filter((item): item is [string, string] => item != null)
+          // sort keys
+          .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+      ),
+      content,
+    ])
+  }
+
+  // tags are not allowed
+  return null
 }
